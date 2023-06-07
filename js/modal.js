4b79cc4a97ca4816583d489a5697207231c0220f
Slider666 <cras.company@gmail.com>
2023-06-07 19:40:18 +0300

    Added main list

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,8 @@
 const refs = {
   openModalMobileMenuBtn: document.querySelector("[data-modal-mobile-menu-open]"),
-
-  // Кнопки регистрации и входа
-  openModalRegisterBtn: document.querySelectorAll("[data-btn-register-open]"),
-  openModalEnterBtn: document.querySelectorAll('[data-btn-enter-open]'),
   
   // Модальное окно мобилки
   modalMobileMenu: document.querySelector("[data-modal-mobile-menu]"),
-
-  // Модальное окно регистрации
-  modalRegister: document.querySelector("[data-modal-register]"),
-
-  // Модальное окно входа
-  modalEnter: document.querySelector('[data-modal-enter]'),
   
   // Кнопки закрытия
   closeModalBtn: document.querySelectorAll('[data-modal-close]'),
@@ -70,14 +60,4 @@ function onCloseModal(modal) {
     });
   }
 
-  addClickHandler(refs.openModalRegisterBtn[1], refs.modalMobileMenu, refs.modalRegister);
   addClickHandler(refs.openModalEnterBtn[1], refs.modalMobileMenu, refs.modalEnter);
-
-// ===========================================================================
-// Открытие и закрытие модального окна регистрации и входа
-// ===========================================================================
-refs.openModalRegisterBtn.forEach((btn) => btn.addEventListener("click", () => onOpenModal(refs.modalRegister)));
-refs.closeModalBtn.forEach((btn) => btn.addEventListener("click", () => onCloseModal(refs.modalRegister)));
-
-refs.openModalEnterBtn.forEach((btn) => btn.addEventListener("click", () => onOpenModal(refs.modalEnter)));
-refs.closeModalBtn.forEach((btn) => btn.addEventListener("click", () => onCloseModal(refs.modalEnter)));
