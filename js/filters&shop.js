26d12c08306bdd34da236b1d7f07b6a4e69af69e
Slider666 <cras.company@gmail.com>
2023-08-28 16:32:09 +0300

    modified loading lazy

--- js/filters&shop.js
+++ js/filters&shop.js
@@ -11,8 +11,9 @@ function createListItemsMarkup(items) {
         
                 <div class="cras-item">
 
-                    <img class="lazyload cras-item__img cras-item--margin"
-                          loadind="lazy"
+                    <img
+                          loading="lazy"
+                          class="lazyload cras-item__img cras-item--margin"
                           data-src="${url}"
                           alt="${alt}" 
                           width="310" 
@@ -258,13 +259,30 @@ function resetMarkup() {
 // Анимация Lazy-loading
 // ===========================================================================
 
-function lazyLoadImagesAnimation() {
-  const lazyImages = document.querySelectorAll('img[class="lazyload cras-item__img cras-item--margin"]');
+// if ('loading' in HTMLImageElement.prototype) {
+//   const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+  
+//   lazyImages.forEach(img => {
+//     img.src = img.dataset.src;
+//   });
+// } else {
+//   const script = document.createElement('script');
 
-  lazyImages.forEach((image) => {
-    image.addEventListener("load", function () {
-      image.classList.add("appear");
-    });
+//   script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js";
+//   script.integrity = "sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==";
+//   script.crossOrigin = "anonymous";
+//   script.referrerpolicy = "no-referrer"
+
+//   document.body.appendChild(script);
+// }
+
+function lazyLoadImagesAnimation() {
+  const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+  lazyImages.forEach(image => {image.addEventListener(
+    "load",
+    function () { image.classList.add("appear") },
+    { once: true });
   });
 }
 
