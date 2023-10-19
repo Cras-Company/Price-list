87e08145c9ce58dd27cc0e922fba64fede06921c
Slider666 <cras.company@gmail.com>
2023-10-19 11:28:38 +0300

    Changed bars

--- js/array-bars.js
+++ js/array-bars.js
@@ -1,6 +1,6 @@
 export const shopLotsBars = [
     {
-        type: "wholesale",
+        type: "retail",
         url: "./images/shop/food/sweets/bars/ferrero_nutella_b_ready_22.jpg",
         alt: "ferrero_nutella_b_ready_22",
         marker: "FDBS030",
@@ -12,8 +12,8 @@ export const shopLotsBars = [
         countryName: "Угорщина",
         weightName: "Вага",
         weight: "22 гр.",
-        priceGRN: 20,
-        priceGRNOpt: 15,
+        priceGRN: 22,
+        quantityOnStorage: 9,
         description01: "&#171Ferrero - Nutella B-ready&#187 - це справжній шедевр для шоколадних гурманів. Цей шоколадний батончик поєднує у собі найкращі якості &#171Nutella&#187 - ніжний крем з горіховим смаком, обгорнутий хрустким печивом.",
         description02: "Цей батончик справжній момент насолоди, який можна відчути у будь-який час. Ідеально підходить для перекусу вдома або у дорозі. Зручна упаковка робить його ідеальним супутником у вашому щоденному житті.",
         description03: "З &#171Ferrero - Nutella B-ready&#187 Ви завжди будете мати солодкий резерв для бадьорого настрою. Вирушайте у свою солодку подорож і робіть її ще яскравішою разом з цим чудовим батончиком.",
@@ -21,7 +21,7 @@ export const shopLotsBars = [
         description05: "Спробуйте &#171Ferrero - Nutella B-ready&#187 вже сьогодні і занурьтесь у світ найсолодших моментів вашого життя!"
     },    
     {
-        type: "wholesale",
+        type: "retail",
         url: "./images/shop/food/sweets/bars/ferrero_nutella_b_ready_44.jpg",
         alt: "ferrero_nutella_b_ready_44",
         marker: "FDBS032",
@@ -33,8 +33,8 @@ export const shopLotsBars = [
         countryName: "Угорщина",
         weightName: "Вага",
         weight: "44 гр.",
-        priceGRN: 35,
-        priceGRNOpt: 27,
+        priceGRN: 45,
+        quantityOnStorage: 16,
         description01: "&#171Ferrero - Nutella B-ready&#187 - це справжній шедевр для шоколадних гурманів. Цей шоколадний батончик поєднує у собі найкращі якості &#171Nutella&#187 - ніжний крем з горіховим смаком, обгорнутий хрустким печивом.",
         description02: "Цей батончик справжній момент насолоди, який можна відчути у будь-який час. Ідеально підходить для перекусу вдома або у дорозі. Зручна упаковка робить його ідеальним супутником у вашому щоденному житті.",
         description03: "З &#171Ferrero - Nutella B-ready&#187 Ви завжди будете мати солодкий резерв для бадьорого настрою. Вирушайте у свою солодку подорож і робіть її ще яскравішою разом з цим чудовим батончиком.",
@@ -42,7 +42,7 @@ export const shopLotsBars = [
         description05: "Спробуйте &#171Ferrero - Nutella B-ready&#187 вже сьогодні і занурьтесь у світ найсолодших моментів вашого життя!"
     },
     {
-        type: "wholesale",
+        type: "retail",
         url: "./images/shop/food/sweets/bars/ferrero_nutella_b_ready_132.jpg",
         alt: "ferrero_nutella_b_ready_132",
         marker: "FDBS034",
@@ -54,8 +54,8 @@ export const shopLotsBars = [
         countryName: "Угорщина",
         weightName: "Вага",
         weight: "132 гр.",
-        priceGRN: 70,
-        priceGRNOpt: 60,
+        priceGRN: 117,
+        quantityOnStorage: 7,
         description01: "&#171Ferrero - Nutella B-ready&#187 - це справжній шедевр для шоколадних гурманів. Цей шоколадний батончик поєднує у собі найкращі якості &#171Nutella&#187 - ніжний крем з горіховим смаком, обгорнутий хрустким печивом.",
         description02: "Цей батончик справжній момент насолоди, який можна відчути у будь-який час. Ідеально підходить для перекусу вдома або у дорозі. Зручна упаковка робить його ідеальним супутником у вашому щоденному житті.",
         description03: "З &#171Ferrero - Nutella B-ready&#187 Ви завжди будете мати солодкий резерв для бадьорого настрою. Вирушайте у свою солодку подорож і робіть її ще яскравішою разом з цим чудовим батончиком.",
