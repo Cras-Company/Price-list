25dca6d284249b46bc11a649e3cb4dd45f633ccb
Slider666 <cras.company@gmail.com>
2023-12-15 13:20:42 +0200

    Added barilla spaghetti

--- js/food/array-spaghetti.js
+++ js/food/array-spaghetti.js
@@ -1,6 +1,27 @@
 export const shopLotsSpaghetti = [
     {
         type: "retail",
+        url: "./images/shop/food/pasta/spaghetti/barilla_spaghetti_5_500.jpg",
+        alt: "barilla_spaghetti_5_500",
+        marker: "FDST050",
+        brand: "Barilla",
+        search: "барила барилла баріла барілла спагетти спагетті твердый твердий сорт тверді сорта твердые сорта с твердых сортов",
+        nameUKR: "Спагеті з твердих сортів",
+        nameEN: "Spaghetti №5",
+        companyName: "Barilla G. e R.",
+        countryName: "Італія",
+        weightName: "Вага",
+        weight: "500 гр.",
+        priceGRN: 55,
+        quantityOnStorage: "9",
+        description01: "Ласкаво просимо у світ справжньої італійської кухні з &#171Barilla - Spaghetti №5&#187.",
+        description02: "Ці тверді спагетті виробляються із найкращих сортів пшениці, що дарує їм особливий аромат і текстуру.",
+        description03: "Підкресліть свою унікальність і відчуйте, як справжній італійський смак відзначається з &#171Barilla - Spaghetti №5&#187.",
+        description04: "<strong>Спагеті з твердих сортів пшениці.</strong>",
+        description05: "Обов'язково спробуйте &#171Barilla - Spaghetti №5&#187 і перетворіть ваші кулінарні творіння у шедеври італійської гастрономії!"
+    },
+    {
+        type: "retail",
         url: "./images/shop/food/pasta/spaghetti/combino_spaghetti_500.jpg",
         alt: "combino_spaghetti_500",
         marker: "FDST100",
