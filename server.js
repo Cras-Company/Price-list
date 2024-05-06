7116d595772548c4e59b3d738f0255d7355b3bfb
Slider666 <cras.company@gmail.com>
2024-05-06 12:14:11 +0300

    Clear

--- server.js
+++ server.js
@@ -7,14 +7,6 @@ const PORT = process.env.PORT || 3000;
 // Указываем Express, где находятся статические файлы
 app.use(express.static(path.join(__dirname)));
 
-// Middleware для установки заголовков HTTP Response для запрета кэширования
-app.use((req, res, next) => {
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', '0');
-    next();
-});
-
 // Маршрут для обработки запросов к вашему сайту
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
