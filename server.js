3b1a21aec7e812105f1aad48667a8340614046f6
Slider666 <cras.company@gmail.com>
2024-05-06 17:16:02 +0300

    Changed about us

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const PORT = process.env.PORT || 3000;
 // Указываем Express, где находятся статические файлы
 app.use(express.static(path.join(__dirname)));
 
+app.use((req, res, next) => {
+
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+
+    next();
+});
+
 // Маршрут для обработки запросов к вашему сайту
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
