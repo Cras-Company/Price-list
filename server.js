feb402d5a8cd62e19d649e4b3c22da221203252b
Slider666 <cras.company@gmail.com>
2024-06-03 11:46:31 +0300

    Changed server

--- server.js
+++ server.js
@@ -4,17 +4,33 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Указываем Express, где находятся статические файлы
-app.use(express.static(path.join(__dirname)));
-
+// Middleware для установки заголовков, предотвращающих кеширование
 app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
   res.setHeader('Pragma', 'no-cache');
   res.setHeader('Expires', '0');
   res.setHeader('Surrogate-Control', 'no-store');
-
   next();
 });
+
+// Middleware для предотвращения кеширования динамических файлов .js
+app.use('/*.js', (req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.setHeader('Surrogate-Control', 'no-store');
+  next();
+});
+
+// Указываем Express, где находятся статические файлы с дополнительными заголовками
+app.use(express.static(path.join(__dirname), {
+  setHeaders: (res, path) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    res.setHeader('Surrogate-Control', 'no-store');
+  }
+}));
 
 // Маршрут для обработки запросов к вашему сайту
 app.get('/', (req, res) => {
