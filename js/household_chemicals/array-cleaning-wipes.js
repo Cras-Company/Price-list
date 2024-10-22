3d6c56083733c61add53d42321fadf3b7d3452b4
Slider666 <cras.company@gmail.com>
2024-10-22 18:00:19 +0300

    Added Lenor & Ombia

--- js/household_chemicals/array-cleaning-wipes.js
+++ js/household_chemicals/array-cleaning-wipes.js
@@ -67,7 +67,7 @@ export const shopLotsCleaningWipes = [
     quantityName: "Кількість",
     quantity: "20 шт.",
     priceGRN: 145,
-    quantityOnStorage: 2,
+    quantityOnStorage: 1,
     description01: "Ваш автомобіль заслуговує на ідеальний догляд, і вологі серветки &#171Denkmit - Cockpit-Pflegetücher Feucht Mit Antistatik-Formel&#187 з антистатичною формулою зроблять це бездоганно.",
     description02: "Вони ефективно видаляють пил і бруд з панелі приладів, залишаючи поверхню чистою і свіжою.",
     description03: "Антистатична формула &#171Denkmit - Cockpit-Pflegetücher Feucht Mit Antistatik-Formel&#187 запобігає повторному накопиченню пилу, що забезпечує тривалий ефект чистоти.",
